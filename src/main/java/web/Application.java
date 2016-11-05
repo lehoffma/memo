f8f574663d85d51442900e60abebaecb6b1e1374
Lenn <lennarthoffmann@gmx.de>
2016-11-05 23:29:58 +0100

    spring init

--- src/main/java/web/Application.java
+++ src/main/java/web/Application.java
@@ -2,20 +2,11 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({WebConfig.class})
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class, WebConfig.class);
     }
 }
--- src/main/java/web/controller/TestController.java
+++ src/main/java/web/controller/TestController.java
@@ -4,20 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//TODO remove
-
-
 @Controller
 public class TestController {
-
-    @RequestMapping("/")
-    public String index(){
-        return "index";
-    }
 
     @RequestMapping("/test")
     @ResponseBody
     public String test(){
-        return "hallo";
+        return "test";
     }
 }
