3dc1608cd5f190f3e8ead29f1d86555f5034f0a8
Lennart Hoffmann <lennarthoffmann@gmx.de>
2017-04-02 20:43:23 +0200

    - Shopping Cart Service now stores its contents in the LocalStorage and retrieves this stored date when first loading the site

--- src/main/web/src/app/shared/services/shopping-cart.service.ts
+++ src/main/web/src/app/shared/services/shopping-cart.service.ts
@@ -1,105 +1,160 @@
-import {Injectable} from "@angular/core";
+import {Injectable, OnInit} from "@angular/core";
 import {EventType} from "../model/event-type";
 import {BehaviorSubject, Observable} from "rxjs";
 
 interface ShoppingCartItem {
-    id: number,
-    amount: number,
-    options?: {
-        size?: string,
-        color?: string,
-    }
+	id: number,
+	amount: number,
+	options?: {
+		size?: string,
+		color?: string,
+	}
 }
 interface ShoppingCartContent {
-    merch: ShoppingCartItem[],
-    partys: ShoppingCartItem[],
-    tours: ShoppingCartItem[]
+	merch: ShoppingCartItem[],
+	partys: ShoppingCartItem[],
+	tours: ShoppingCartItem[]
 }
 
 
 @Injectable()
-export class ShoppingCartService {
-    private _content: BehaviorSubject<ShoppingCartContent> = new BehaviorSubject({
-        merch: [{id: 0, amount: 1, options: {size: "XS", color: "red"}},
-            {id: 1, amount: 1, options: {size: "XS", color: "red"}},
-            {id: 0, amount: 1, options: {size: "S", color: "red"}}],
-        partys: [{id: 1, amount: 1}, {id: 1, amount: 1}],
-        tours: [{id: 0, amount: 1}, {id: 1, amount: 2}]
-    });
-    public content = this._content.asObservable();
+export class ShoppingCartService implements OnInit {
+	//todo löschen wenn test-daten nicht mehr benötigt werden
+	// private _content: BehaviorSubject<ShoppingCartContent> = new BehaviorSubject({
+	// 	merch: [{id: 0, amount: 1, options: {size: "XS", color: "red"}},
+	// 		{id: 1, amount: 1, options: {size: "XS", color: "red"}},
+	// 		{id: 0, amount: 1, options: {size: "S", color: "red"}}],
+	// 	partys: [{id: 1, amount: 1}, {id: 1, amount: 1}],
+	// 	tours: [{id: 0, amount: 1}, {id: 1, amount: 2}]
+	// });
+
+	private _content: BehaviorSubject<ShoppingCartContent> = new BehaviorSubject({
+		merch: [],
+		partys: [],
+		tours: []
+	});
+	public content = this._content.asObservable();
 
 
-    constructor() {
+	constructor() {
+		this.initFromLocalStorage();
+	}
 
-    }
+	ngOnInit() {
+	}
 
-    get amountOfCartItems(): Observable<number> {
-        return this.content.map(content => [...content.merch, ...content.partys, ...content.tours]
-            .reduce((previousValue, currentValue) => previousValue + currentValue.amount, 0))
-    }
+	get amountOfCartItems(): Observable<number> {
+		return this.content.map(content => [...content.merch, ...content.partys, ...content.tours]
+			.reduce((previousValue, currentValue) => previousValue + currentValue.amount, 0))
+	}
 
-    /**
-     *
-     * @param type die Art des Events (entweder 'merch', 'tours' oder 'partys')
-     * @param id die ID des Items, welches entfernt werden soll
-     * @param content das content-objekt, aus dem das Item entfernt werden soll
-     * @returns {ShoppingCartContent}
-     */
-    private remove(type: EventType, id: number, content: ShoppingCartContent) {
-        let itemIndex = content[type].findIndex(cardItem => cardItem.id === id);
-        if (itemIndex) {
-            //remove
-            content[type].splice(itemIndex, 1);
-        }
-        //das objekt gibt es nicht :(
-        return content;
-    }
+	/**
+	 *
+	 * @param type die Art des Events (entweder 'merch', 'tours' oder 'partys')
+	 * @param id die ID des Items, welches entfernt werden soll
+	 * @param content das content-objekt, aus dem das Item entfernt werden soll
+	 * @returns {ShoppingCartContent}
+	 */
+	private remove(type: EventType, id: number, content: ShoppingCartContent) {
+		let itemIndex = content[type].findIndex(cardItem => cardItem.id === id);
+		if (itemIndex) {
+			//remove
+			content[type].splice(itemIndex, 1);
+		}
+		//das objekt gibt es nicht :(
+		return content;
+	}
 
-    /**
-     *
-     * @param type die Art des Events (entweder 'merch', 'tours' oder 'partys')
-     * @param item das Item, welches hinzugefügt werden soll
-     * @param content das content-objekt, zu dem das Item hinzugefügt werden soll
-     * @returns {ShoppingCartContent}
-     */
-    private add(type: EventType, item: ShoppingCartItem, content: ShoppingCartContent) {
-        //Vergleicht ob das Objekt den gleichen inhalt hat.
-        let itemIndex = content[type].findIndex(cardItem => {
-            return cardItem.id === item.id
-                && cardItem.options
-                && cardItem.options.size === item.options.size
-                && cardItem.options.color === item.options.color
-        });
-        //Wenn ja, wird die Anzahl erhöht..
-        if (itemIndex !== -1) {
-            content[type][itemIndex].amount += item.amount;
-        }
-        //wenn nein, wird das item hinzugefügt
-        else {
-            content[type].push(item);
-        }
-        return content;
-    }
+	/**
+	 *
+	 * @param type die Art des Events (entweder 'merch', 'tours' oder 'partys')
+	 * @param item das Item, welches hinzugefügt werden soll
+	 * @param content das content-objekt, zu dem das Item hinzugefügt werden soll
+	 * @returns {ShoppingCartContent}
+	 */
+	private add(type: EventType, item: ShoppingCartItem, content: ShoppingCartContent) {
+		//Vergleicht ob das Objekt den gleichen inhalt hat.
+		let itemIndex = content[type].findIndex(cardItem => {
+			return cardItem.id === item.id
+				&& cardItem.options
+				&& cardItem.options.size === item.options.size
+				&& cardItem.options.color === item.options.color
+		});
+		//Wenn ja, wird die Anzahl erhöht..
+		if (itemIndex !== -1) {
+			content[type][itemIndex].amount += item.amount;
+		}
+		//wenn nein, wird das item hinzugefügt
+		else {
+			content[type].push(item);
+		}
+		return content;
+	}
 
-    /**
-     *
-     * @param type die Art des Events (entweder 'merch', 'tours' oder 'partys')
-     * @param item das Item welches hinzugefügt werden soll
-     */
-    public addItem(type: EventType, item: ShoppingCartItem) {
-        let newValue = this.add(type, item, this._content.value);
-        this._content.next(newValue);
-    }
+	/**
+	 *
+	 * @param type die Art des Events (entweder 'merch', 'tours' oder 'partys')
+	 * @param item das Item welches hinzugefügt werden soll
+	 */
+	public addItem(type: EventType, item: ShoppingCartItem) {
+		let newValue = this.add(type, item, this._content.value);
+		this.pushNewValue(newValue);
+	}
 
-    /**
-     *
-     * @param type die Art des Events (entweder 'merch', 'tours' oder 'partys')
-     * @param id die ID des Items, welches gelöscht werden soll
-     */
-    public deleteItem(type: EventType, id: number) {
-        let newValue = this.remove(type, id, this._content.value);
-        this._content.next(newValue);
-    }
+	/**
+	 *
+	 * @param type die Art des Events (entweder 'merch', 'tours' oder 'partys')
+	 * @param id die ID des Items, welches gelöscht werden soll
+	 */
+	public deleteItem(type: EventType, id: number) {
+		let newValue = this.remove(type, id, this._content.value);
+		this.pushNewValue(newValue);
+	}
 
+	/**
+	 * Pusht die gegebenen Daten in das interne Content Object, so dass alle Subscriber
+	 * Methoden mit den neuen Daten aufgerufen werden
+	 * @param newValue
+	 */
+	private pushNewValue(newValue: ShoppingCartContent) {
+		this._content.next(newValue);
+		this.saveToLocalStorage(this._content.value);
+	}
 
+	/**
+	 * Initialisiert das _content Objekt mit den im LocalStorage gespeicherten Daten
+	 */
+	private initFromLocalStorage() {
+		this.pushNewValue(this.getContentFromLocalStorage());
+	}
+
+	/**
+	 * Der Key, der für das Speichern des ShoppingCartContents im LocalStorage verwendet wird
+	 * @type {string}
+	 */
+	private readonly localStorageKey = "shoppingCart";
+
+	/**
+	 * Speichert das gegebene ShoppingCartContent objekt im LocalStorage
+	 * @param content
+	 */
+	private saveToLocalStorage(content: ShoppingCartContent) {
+		localStorage.setItem(this.localStorageKey, JSON.stringify(content));
+	}
+
+	/**
+	 * Holt die gespeicherten ShoppingCartContent Daten aus dem LocalStorage des Browsers.
+	 * TODO: speichern, wann es das letzte mal gespeichert wurde und löschen, falls zu lange her (1 tag?)
+	 * @returns {any}
+	 */
+	private getContentFromLocalStorage(): ShoppingCartContent {
+		let json: string = localStorage.getItem(this.localStorageKey);
+
+		//falls nichts im localStorage gespeichert wurde, ist der Warenkorb wohl leer
+		if (!json) {
+			return {partys: [], tours: [], merch: []};
+		}
+
+		return JSON.parse(json);
+	}
 }
