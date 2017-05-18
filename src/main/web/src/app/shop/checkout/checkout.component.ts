f109f809234b31a816be5098a2d85c2b0651dbd2
lamprecht <lamprecht.pia@googlemail.com>
2017-05-18 15:25:06 +0200

    idee bezahlung

--- src/main/web/src/app/shop/checkout/checkout.component.ts
+++ src/main/web/src/app/shop/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
+import {User} from "../../shared/model/user";
+import {UserService} from "../../shared/services/user.service";
+import {Observable} from "rxjs/Observable";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
 	selector: "memo-checkout",
@@ -6,11 +10,17 @@ import {Component, OnInit} from "@angular/core";
 	styleUrls: ["./checkout.component.scss"]
 })
 export class CheckoutComponent implements OnInit {
+	//*ngIf="userObservable | async as user"
+	userObservable: Observable<User>;
+	constructor(//private route: ActivatedRoute, private userService: UserService
+		 ) {
 
-	constructor() {
 	}
 
 	ngOnInit() {
+		//const userId = this.route.params.map(params => +params["id"]);
+		//this.userObservable = userId.flatMap(id => this.userService.getById(id));
+
 	}
 
 }
