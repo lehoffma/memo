84e88b8b6d6c10f25282d258f16762070da53770
Nils <nils.poecking@hotmail.de>
2018-03-21 21:57:32 +0100

    Mails

---
+++ src/main/java/memo/api/EmailServlet.java
@@ -0,0 +1,22 @@
+package memo.api;
+
+import memo.util.SendMail;
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "EmailServlet", value = "/api/email")
+public class EmailServlet extends HttpServlet {
+
+
+    protected Logger logger = Logger.getLogger(EmailServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SendMail.getInstance().send("lennarthoffmann@gmx.de","Hallo Lennart!","Mails verschicken scheint zu funktionieren. Sag mal bitte was bei Ümlauten passiert :D");
+    }
+}
