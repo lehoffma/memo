dc8512c00cc69eaac50225b21945f7d5d7506f54
Nils <nils.poecking@hotmail.de>
2018-04-02 21:40:38 +0200

    Removed Email Account Data

--- src/main/java/memo/util/SendMail.java
+++ src/main/java/memo/util/SendMail.java
@@ -23,20 +23,14 @@ public class SendMail {
         return instance;
     }
 
-    SendMail() {
+    SendMail() {}
 
-        HOST = "smtp.strato.de";
-        USER = "shop@meilenwoelfe.de";
-        USER_NAME = "Meilenshop";
-        PASSWORD = "Wh2k15nb,e!";
-        PORT = "465";
-    }
+    private String host;
+    private String user;
+    private String userName;
+    private String password;
+    private String port;
 
-    private String HOST;
-    private String USER;
-    private String USER_NAME;
-    private String PASSWORD;
-    private String PORT;
 
     private void logEmail(Priority priority, String to, String subject, String text) {
         logger.log(priority, "recipient: " + to);
@@ -48,15 +42,15 @@ public class SendMail {
 
         //Get the session object
         Properties props = new Properties();
-        props.put("mail.smtp.host", HOST);
+        props.put("mail.smtp.host", getHost());
         props.put("mail.smtp.auth", "true");
-        //props.put("mail.smtp.user", USER);
-        //props.put("mail.smtp.port", PORT);
+        props.put("mail.smtp.user", getUser());
+        props.put("mail.smtp.port", getPort());
 
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(USER, PASSWORD);
+                        return new PasswordAuthentication(getUser(), getPassword());
                     }
                 });
 
@@ -64,7 +58,7 @@ public class SendMail {
         try {
 
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(USER, USER_NAME));
+            message.setFrom(new InternetAddress(getUser(), getUserName()));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject(subject);
             message.setContent(text,"text/html");
@@ -88,5 +82,36 @@ public class SendMail {
                 throw e;
             }
         }
+    }
+
+    public String getHost() {
+        return host;
+    }
+    public void setHost(String host) {
+        this.host = host;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPort() {
+        return port;
+    }
+    public void setPort(String port) {
+        this.port = port;
     }
 }
