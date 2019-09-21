f9a998bbefe4dc02c2231d4034502a9bc79e078a
Lennart Hoffmann <lennarthoffmann@gmx.de>
2019-09-21 15:07:34 +0200

    removed test step from build-web github action + changed docker image name to private repository one + removed arm32 docker files

--- src/main/web/src/app/shared/progressive-image-loading/progressive-image-src.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ProgressiveImageSrcDirective } from './progressive-image-src.directive';
-
-describe('ProgressiveImageSrcDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ProgressiveImageSrcDirective();
-    expect(directive).toBeTruthy();
-  });
-});
