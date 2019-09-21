7446c74bf123da30ecf0e51d84431439000dc5be
Lennart Hoffmann <lennarthoffmann@gmx.de>
2019-09-21 18:22:06 +0200

    debugging of write-key-to-env-file

--- src/main/web/src/write-key-to-env-file.js
+++ src/main/web/src/write-key-to-env-file.js
@@ -5,18 +5,20 @@ const GOOGLE_ANALYTICS_KEY = process.env.GOOGLE_ANALYTICS_KEY;
 //this script writes the maps/analytics api keys from the CI environment to the environment files so we can use them
 //in the app
 try {
-	const options = {
+	console.log(GOOGLE_MAPS_API_KEY.slice(0,4));
+	let changedFiles = replace.sync({
 		files: 'environments/*.ts',
-		from: /\{GOOGLE_MAPS_API_KEY\}/g,
+		from: /{GOOGLE_MAPS_API_KEY}/g,
 		to: GOOGLE_MAPS_API_KEY,
 		allowEmptyPaths: false,
-	};
-	let changedFiles = replace.sync(options);
+	});
 
-	options.from = /\{GOOGLE_ANALYTICS_KEY\}/g;
-	options.to = GOOGLE_ANALYTICS_KEY;
-
-	changedFiles.push(...replace.sync(options));
+	changedFiles.push(...replace.sync({
+		files: 'environments/*.ts',
+		from: /{GOOGLE_ANALYTICS_KEY}/g,
+		to: GOOGLE_ANALYTICS_KEY,
+		allowEmptyPaths: false,
+	}));
 
 	console.log("changed files: " +  changedFiles.join(","));
 
